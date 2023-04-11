cb0ae61b496f2630c1c3b0d43a81d818c0a44d06
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-11 19:53:40 +0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", function(){
     var footer = document.getElementById("myFooter");
     if(window.pageYOffset > 500){
         footer.classList.add("show-footer"); 
+        this.document.getElementById("side-menu").style.display = 'none';
     } else {
         footer.classList.remove("show-footer");
     }
