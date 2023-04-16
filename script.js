b493cfddb1bae3f509c228f9d3d927d80749a414
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-17 01:26:11 +0300

    menu animations

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    sideMenu.style.left = '0';
-    mainContent.classList.add('shifted-right');
-  });
-  
 const showMenuButton = document.getElementById('show-menu');
 const hideMenuButton = document.getElementById('hide-menu');
 const sideMenu = document.getElementById('side-menu');
-const mainContent = document.getElementsByClassName('main-content')[0]; 
+const mainContent = document.getElementsByClassName('main-content')[0];
+const footer = document.getElementById("my-footer");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    sideMenu.style.left = '0';
+    mainContent.classList.add('shifted-right');
+    footer.style.display = 'none';
+
+});
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 500) {
+        footer.style.display = 'block';
+        sideMenu.classList.add('fade-out-left');
+        sideMenu.classList.remove('fade-in-right');
+        mainContent.classList.remove('shifted-right');
+    } else {
+        sideMenu.classList.add('fade-in-right');
+        sideMenu.classList.remove('fade-out-left');
+        mainContent.classList.add('shifted-right');
+        footer.style.display = 'none';
+    }
+})
 
 showMenuButton.addEventListener('click', () => {
-  sideMenu.style.left = '0';
-  showMenuButton.classList.remove('flip-button');
-  hideMenuButton.classList.add('flip-button');
-  mainContent.classList.add('shifted-right'); 
+    sideMenu.classList.add('fade-in-right');
+    sideMenu.classList.remove('fade-out-left');
+    showMenuButton.classList.remove('flip-button');
+    hideMenuButton.classList.add('flip-button');
+    mainContent.classList.add('shifted-right');
 });
 
 hideMenuButton.addEventListener('click', () => {
-  sideMenu.style.left = '-202px';
-  showMenuButton.classList.add('flip-button');
-  hideMenuButton.classList.remove('flip-button');
-  mainContent.classList.remove('shifted-right');
+    sideMenu.classList.add('fade-out-left');
+    sideMenu.classList.remove('fade-in-right');
+    showMenuButton.classList.add('flip-button');
+    hideMenuButton.classList.remove('flip-button');
+    mainContent.classList.remove('shifted-right');
 });
+
+
+
