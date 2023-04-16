7056f8de48d2cf5e6aeca4b6a213e7f6a5ff551e
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-16 23:17:02 +0300

    toggle menu

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// Get the menu toggle button
-var menuToggle = document.getElementById('menu-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+    sideMenu.style.left = '0';
+    showMenuButton.classList.add('flip-button');
+    mainContent.classList.add('shifted-right');
+  });
+  
+const showMenuButton = document.getElementById('show-menu');
+const hideMenuButton = document.getElementById('hide-menu');
+const sideMenu = document.getElementById('side-menu');
+const mainContent = document.getElementsByClassName('main-content')[0]; 
 
-// Get the side menu
-var sideMenu = document.getElementById('side-menu');
+showMenuButton.addEventListener('click', () => {
+  sideMenu.style.left = '0';
+  showMenuButton.classList.add('flip-button');
+  mainContent.classList.add('shifted-right'); // add the new class to the main content
+});
 
-// Add a click event listener to the menu toggle button
-menuToggle.addEventListener('click', function() {
-	// Toggle the "show-menu" class on the body element
-	document.body.classList.toggle('show-menu');
+hideMenuButton.addEventListener('click', () => {
+  sideMenu.style.left = '-205px';
+  mainContent.classList.remove('shifted-right'); // remove the class when hiding the menu
 });
