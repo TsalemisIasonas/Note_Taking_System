b60d140f82bf51d8ab0a55851f0af4c142e7dc78
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-17 04:00:10 +0300

    accordions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const hideMenuButton = document.getElementById('hide-menu');
 const sideMenu = document.getElementById('side-menu');
 const mainContent = document.getElementsByClassName('main-content')[0];
 const footer = document.getElementById("my-footer");
+const addAccordionButton = document.getElementById("add-accordion-button");
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -43,4 +44,48 @@ hideMenuButton.addEventListener('click', () => {
 });
 
 
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
+addAccordionButton.addEventListener("click",function(){
+    const accordionContainer = document.createElement('div');
+    const accordionButton = document.createElement('button');
+    const accordionTitle = document.createElement('input');
+    const accordionPanel = document.createElement('div');
+    const accordionTextArea = document.createElement('textarea');
+    
+    accordionTitle.placeholder = 'Title...';
+    accordionTextArea.placeholder = 'Content...';
+
+    accordionContainer.classList.add('accordion-container');
+    accordionButton.classList.add('accordion');
+    accordionTitle.classList.add('accordion-input');
+    accordionPanel.classList.add('panel');
+    accordionTextArea.classList.add('panel-content');
+    
+    
+    mainContent.appendChild(accordionContainer);
+    accordionContainer.appendChild(accordionButton);
+    accordionContainer.appendChild(accordionPanel);
+    accordionButton.appendChild(accordionTitle);
+    accordionPanel.appendChild(accordionTextArea);
+
+})
 
