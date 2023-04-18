362d29b9ecbbd50fe468663a5b3f31342416410a
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-19 02:24:42 +0300

    Completed function format

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const showMenuButton = document.getElementById('show-menu'),
-      hideMenuButton = document.getElementById('hide-menu'),
-      sideMenu = document.getElementById('side-menu'),
-      sideMenuList = sideMenu.getElementsByClassName('options-list')[0],
-      mainContent = document.querySelector('.main-content'),
-      mainMenu = document.querySelector('.notes-menu'),
-      mainMenuElements = mainMenu.querySelector('.notes-menu-elements'),
-      footer = document.getElementById("my-footer"),
-      addOptionButton = document.getElementById('add_option_button'),
-      addAccordionButton = document.getElementById("add-accordion-button"),
-      accordionButtons = document.getElementsByClassName("accordion"),
-      deleteAccordionIcons = document.querySelectorAll('.delete-accordion, .fas, .fa-trash, .fa-bounce'),
-      categoryLinks = document.querySelectorAll('a[data-category]');
+    hideMenuButton = document.getElementById('hide-menu'),
+    sideMenu = document.getElementById('side-menu'),
+    sideMenuList = sideMenu.getElementsByClassName('options-list')[0],
+    mainContent = document.querySelector('.main-content'),
+    mainMenu = document.querySelector('.notes-menu'),
+    mainMenuElements = mainMenu.querySelector('.notes-menu-elements'),
+    footer = document.getElementById("my-footer"),
+    addOptionButton = document.getElementById('add_option_button'),
+    addAccordionButton = document.getElementById("add-accordion-button"),
+    accordionButtons = document.querySelectorAll(".accordion"),
+    deleteAccordionIcons = document.querySelectorAll('.delete-accordion, .fas, .fa-trash, .fa-bounce'),
+    categoryLinks = document.querySelectorAll('a[data-category]');
 
+let optionsContainer = document.querySelector('.options-container'),
+    optionsList = document.querySelector('.options-list');
+
+// page loads
 document.addEventListener('DOMContentLoaded', () => {
     sideMenu.style.left = '0';
     mainContent.classList.add('shifted-right');
@@ -19,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     footer.style.display = 'none';
 });
 
+// show footer if user scrolls under all elements
 window.addEventListener("scroll", function () {
     if (window.pageYOffset > 1000) {
         footer.style.display = 'block';
@@ -38,6 +43,7 @@ window.addEventListener("scroll", function () {
     }
 });
 
+// show & hide side menu
 showMenuButton.addEventListener('click', () => {
     showSideMenu(true);
 });
@@ -59,24 +65,66 @@ function showSideMenu(show) {
     mainMenuElements.classList.toggle('fullscreen-width', !show);
 }
 
+// update category text color
+categoryLinks.forEach(category => category.addEventListener('click', () => {
+    highlight(category);
+}));
+
+function highlight(element) {
+    elementCategory = element.getAttribute('data-category');
+    element.classList.add('selected');
+    document.getElementById('notes-menu-header').innerHTML = 'My Notes - ' + elementCategory;
+    for (var i = 0; i < document.querySelectorAll('a[data-category]').length; i++) {
+        if (document.querySelectorAll('a[data-category]')[i] != element) {
+            document.querySelectorAll('a[data-category]')[i].classList.remove('selected');
+        }
+    }
+}
+
+// create new categories
 addOptionButton.addEventListener('click', addNewOption);
 
 function addNewOption() {
-    let optionsContainer = document.querySelector('.options-container'),
-        optionsList = document.querySelector('.options-list'),
-        newListItem = document.createElement('li'),
+    let newListItem = document.createElement('li'),
         newCategory = document.createElement('a');
 
-    newCategory.setAttribute('href','#');
-    newCategory.setAttribute('data-category','New Option');
+    newCategory.setAttribute('href', '#');
+    newCategory.setAttribute('data-category', 'New Option');
     newCategory.textContent = 'New Option';
+
     newListItem.appendChild(newCategory);
-    optionsList.insertBefore(newListItem,optionsList.firstChild);
+    optionsList.insertBefore(newListItem, optionsList.firstChild);
     optionsContainer.appendChild(optionsList);
 
-    accordionButtons.forEach(button => button.addEventListener("click", showPanel));
-    deleteAccordionIcons.forEach(icon => icon.addEventListener('click', deleteAccordion));
-    categoryLinks.forEach(link => link.addEventListener('click', selectCategory));
+    newCategory.addEventListener('click', () => {
+        highlight(newCategory);
+    });
+    newCategory.addEventListener('click', () => {
+        selectCategory(newCategory.dataset.category);
+    });
+
+}
+
+
+// show only active category, operate on the accordions
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("accordion")) { // Check if the clicked element is an accordion
+        showPanel(event.target);
+    }
+});
+deleteAccordionIcons.forEach(icon => icon.addEventListener('click', deleteAccordion));
+document.querySelectorAll('a[data-category]').forEach(a => a.addEventListener('click', function () {
+    selectCategory(this.dataset.category);
+}));
+
+function showPanel(element) {
+    element.classList.toggle('active');
+    let panel = element.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
 }
 
 function deleteAccordion(e) {
@@ -85,22 +133,8 @@ function deleteAccordion(e) {
     container.parentNode.removeChild(container);
 }
 
-function selectCategory(e) {
-    let constMenuHeader = document.querySelector('#notes-menu-header');
-    e.preventDefault();
-    const category = this.getAttribute('data-category');
-
-    this.classList.add('selected');
-    constMenuHeader.textContent = `My Notes - ${category}`;
-
-    categoryLinks.forEach(link => {
-        if (this !== link){
-            link.classList.remove('selected');
-        }
-    });
-
-    var notes = document.querySelectorAll('div[data-category-notes]');
-
+function selectCategory(category) {
+    let notes = document.querySelectorAll('div[data-category-notes]');
     notes.forEach(note => note.style.display = 'none');
 
     const notesToShow = document.querySelector(`div[data-category-notes="${category}"]`);
@@ -110,21 +144,17 @@ function selectCategory(e) {
     }
 }
 
-accordionButtons.forEach(button => button.addEventListener("click", showPanel));
-deleteAccordionIcons.forEach(icon => icon.addEventListener('click', deleteAccordion));
-categoryLinks.forEach(link => link.addEventListener('click', selectCategory));
-
 addAccordionButton.addEventListener("click", addNewAccordion);
 
 function addNewAccordion() {
     const category = document.querySelector('.selected').getAttribute('data-category');
 
     const accordionContainer = document.createElement('div'),
-          accordionButton = document.createElement('button'),
-          accordionTitle = document.createElement('input'),
-          deleteAccordionIcon = document.createElement('i'),
-          accordionPanel = document.createElement('div'),
-          accordionTextArea = document.createElement('textarea');
+        accordionButton = document.createElement('button'),
+        accordionTitle = document.createElement('input'),
+        deleteAccordionIcon = document.createElement('i'),
+        accordionPanel = document.createElement('div'),
+        accordionTextArea = document.createElement('textarea');
 
     deleteAccordionIcon.classList.add('delete-accordion', 'fas', 'fa-trash', 'fa-bounce');
     deleteAccordionIcon.addEventListener('click', deleteAccordion);
@@ -147,7 +177,9 @@ function addNewAccordion() {
 
     const newAccordions = document.querySelectorAll(".accordion");
 
-    newAccordions.forEach(button => button.addEventListener("click", showPanel));
+    newAccordions.forEach(button => button.addEventListener("click", () => {
+        showPanel(button);
+    }));
 
     let categoryNote = document.querySelector(`div[data-category-notes="${category}"]`);
 
@@ -157,7 +189,7 @@ function addNewAccordion() {
     }
     else {
         categoryNote = document.createElement('div');
-        categoryNote.setAttribute('data-category-notes',category);
+        categoryNote.setAttribute('data-category-notes', category);
         categoryNote.appendChild(accordionContainer);
         mainContent.appendChild(categoryNote);
     }
